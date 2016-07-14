79f68b8c1392a5112d8d1a2fc15a738febd253de
wind <gadyang83@gmail.com>
2016-07-14 15:24:36 +0900

    test

---
+++ sock/wesock.h
@@ -0,0 +1,149 @@
+/*
+ * w_esocket.h
+ *
+ *  Created on: 2014. 12. 30.
+ *      Author: wind
+ */
+
+#ifndef W_ESOCKET_H_
+#define W_ESOCKET_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/epoll.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <pthread.h>
+
+#include <syslog.h>
+
+struct wsock;
+struct wsock_table;
+
+enum {
+	WSOCK_NONE=0,
+	WSOCK_TCP_SERVER,
+	WSOCK_TCP_CLIENT,
+	WSOCK_TCP_CLIENT_SINGLE
+};
+
+#define WSOCK_ADDR_IP_STRLEN_MAX 40
+struct wsock_addr
+{
+	int flag_v6;
+	union {
+		struct sockaddr_in v4;
+	};
+	char ch_ip[WSOCK_ADDR_IP_STRLEN_MAX];
+	unsigned short int h_port; ///< port : host order
+};
+
+struct wsock
+{
+	int sock;
+	struct epoll_event ep_event;
+	int type;
+	struct wsock_addr addr_info;
+	unsigned char *buff;
+	int offset;
+	int read_len;
+	int buff_size;
+	union wsock_data {
+		struct {
+			unsigned long int recv_bytes;
+			unsigned long int sent_bytes;
+			struct wsock *server;
+		} client;
+		struct {
+			int client_count_current;
+			int client_count_max;
+		} server;
+	} data;
+	time_t time_begin;
+	time_t time_end;
+
+	// User's data
+	void *expand_data;
+	void *user_data1;
+	void *user_data2;
+	// end User's data
+
+	void (*fn_connection) (struct wsock_table *, struct wsock *, struct wsock *);
+	void (*fn_receive) (struct wsock_table *, struct wsock *, unsigned char *, int, int *);
+	void (*fn_disconnection) (struct wsock_table *, struct wsock *);
+	int flag_in_pool;
+	struct wsock_table *table;
+	struct wsock *prev;
+	struct wsock *next;
+};
+
+#define WSOCK_TABLE_NAME_STRLEN_MAX 128
+struct wsock_table
+{
+	char name[WSOCK_TABLE_NAME_STRLEN_MAX];
+	pthread_spinlock_t lock;
+	int epoll_fd;
+
+	int flag_running;
+	int flag_exit;
+	int timeout;
+	int element_count_current;
+	int pool_count;
+	int element_count_max;
+	int q_size;
+	int q_front;
+	int q_rear;
+	struct wsock **wsock_release_q;
+
+	struct epoll_event *ep_event;
+	struct wsock *wsock_mem;
+	struct wsock wsock_pool;
+	struct wsock wsock_head;
+	unsigned char *buff_mem;
+};
+
+// Start Table
+int wsock_table_run(struct wsock_table *table);
+
+// Release Table
+void wsock_release_tcp_table(struct wsock_table *table, int element_count, int  buff_size, int timeout);
+
+// Create Table
+int wsock_create_tcp_table(struct wsock_table *table, int element_count, int buff_size, int timeout);
+
+// Add New TCP Client
+struct wsock *wsock_add_new_tcp_client(
+		struct wsock_table *table, char *str_serv_addr, unsigned short int serv_port, unsigned short int clnt_port, int timeout, void *expand_data,
+		void (*fn_receive) (struct wsock_table *, struct wsock *, unsigned char *, int, int *),
+		void (*fn_disconnection) (struct wsock_table *, struct wsock *));
+
+// Add New TCP Server
+int wsock_add_new_tcp_server(
+		struct wsock_table *table, char *str_addr, unsigned short int port, int backlog, int client_count, void *expand_data,
+		void (*fn_connection) (struct wsock_table *, struct wsock *, struct wsock *),
+		void (*fn_receive) (struct wsock_table *, struct wsock *, unsigned char *, int, int *),
+		void (*fn_disconnection) (struct wsock_table *, struct wsock *));
+// Clear wsock
+void wsock_memset(struct wsock *wsock);
+
+// Connection Release
+void wsock_close(struct wsock *wsock);
+
+// Convert a String address to a Data address
+int wsock_conv_str_address(char *str_addr, int str_addr_len, unsigned short int port, struct wsock_addr *serv_info);
+
+// Client Connector
+int wsock_connect_wait(int sockfd, struct sockaddr *saddr, int addrsize, int sec);
+
+// Send Data
+int wsock_send(struct wsock *wsock, const void *buff, int len);
+#endif /* W_ESOCKET_H_ */
---
+++ sock/wesock_test_client.c
@@ -0,0 +1,65 @@
+#include "wesock.h"
+
+void f_recv (struct wsock_table *table, struct wsock *clnt, unsigned char *buff, int read_len, int *offset)
+{
+	printf("%s:%d receive data(%d bytes).\n", clnt->addr_info.ch_ip, clnt->addr_info.h_port, read_len);
+}
+
+void f_disconn (struct wsock_table *table, struct wsock *clnt)
+{
+	printf("%s:%d is disconnected.\n", clnt->addr_info.ch_ip, clnt->addr_info.h_port);
+}
+
+void *f_runner(void *obj)
+{
+	struct wsock_table *table = (struct wsock_table *) obj;
+
+	printf("Excute Table!!!\n");
+	wsock_table_run(table);
+	printf("Stop Table!!!\n");
+
+	return NULL;
+}
+
+int main()
+{
+	struct wsock_table table;
+	struct wsock *wsock;
+	pthread_t pid;
+	char *buff;
+	int buff_len, i;
+
+	buff = malloc(1024 * 1024 * 10);
+	if(buff == NULL) {
+		printf("malloc() is failed.\n");
+		return -1;
+	}
+
+	if(wsock_create_tcp_table(&table, 1, 1024 * 1024, 60) < 0) {
+		printf("wsock_create_tcp_table() is failed.\n");
+		return -1;
+	}
+
+	if((wsock = wsock_add_new_tcp_client(&table, "15.1.2.91", 10348, 0, 5, NULL,
+			f_recv,
+			f_disconn)) == NULL) {
+		printf("wsock_add_new_tcp_client() is failed.\n");
+		return -1;
+	}
+	pthread_create(&pid, NULL, f_runner, (void *) &table);
+	sleep(1);
+
+	srand((unsigned int)time(NULL));
+	for(i=0; i<10; i++) {
+		buff_len = (rand() % (1024 * 1024 * 10)) + 1;
+		printf("buff_len : %d\n", buff_len);
+		if(wsock_send(wsock, buff, buff_len) < 0) {
+			printf("wsock_send() is failed.\n");
+			wsock->table->flag_exit = 1;
+			break;
+		}
+	}
+	wsock_conn_release(wsock);
+
+	return 0;
+}
